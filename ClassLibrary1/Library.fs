1e44dbd1cca982c6308dd3c655ca67d161352289
kirichenko <vaskir@gmail.com>
2018-12-25 12:34:45 +0300

    init

---
+++ ClassLibrary1/Library.fs
@@ -0,0 +1,6 @@
+﻿module ClassLibrary1.Client
+open ClassLibrary8
+
+let _ =
+    if Class1.IsMached "foo" then ()
+    else ()
---
+++ ClassLibrary8/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace ClassLibrary8
+{
+    public static class Class1
+    {
+        public static bool IsMached(string s) => true;
+    }
+}
